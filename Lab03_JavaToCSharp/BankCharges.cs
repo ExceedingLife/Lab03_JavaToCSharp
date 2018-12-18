20c897149d119b8d54ebbd08084408bdf214fa9a
Andrew Harkins <extremelyexceeding@gmail.com>
2018-12-17 23:07:18 -0600

    BankDemo + BankCharges : Console Application
    BankCharges class that takes balance and # of checks.
    Does calculations depending on amounts
    
    BankChargesDemo is the Main which runs in the console.

---
+++ Lab03_JavaToCSharp/BankCharges.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_JavaToCSharp
+{
+    class BankCharges
+    {
+        // Creating all the class fields.
+        private static readonly int baseCharge = 10;
+        private static readonly int extraCharge = 15;
+        private double balance = 0;
+        private double bankFees = 0;
+        private double checkFees = 0;
+        private int checks = 0;
+
+        // Constructors
+        public BankCharges() { }
+
+        public BankCharges(double b, int c)
+        {
+            balance = b;
+            checks = c;
+        }
+
+        // GetFees method for calculations
+        public double GetFees()
+        {
+            if(checks < 20)
+            {
+                checkFees = .10;
+            }
+            else if(checks >= 20 && checks <= 39)
+            {
+                checkFees = .08;
+            }
+            else if(checks >= 40 && checks <= 59)
+            {
+                checkFees = .06;
+            }
+            else
+            {
+                checkFees = .04;
+            }
+            // If balance is less than 400;
+            if(balance < 400)
+            {
+                bankFees = baseCharge + extraCharge + (checkFees * checks);
+            }
+            else
+            {
+                bankFees = baseCharge + (checkFees * checks); // Account balance > 400;
+            }
+            // return the fees price.
+            return bankFees;
+        }
+
+    }
+}
--- Lab03_JavaToCSharp/Program.cs
+++ Lab03_JavaToCSharp/Program.cs
@@ -8,8 +8,46 @@ namespace Lab03_JavaToCSharp
 {
     class Program
     {
+        // BankChargesDemo Program.
         static void Main(string[] args)
         {
+            int checks = 0;     //Number of checks written.
+            double balance = 0; //Account balance.
+
+            // Get the account balance.
+            Console.WriteLine("What is the starting account balance? ");
+            string input0 = Console.ReadLine();
+            try
+            {                
+                balance = Convert.ToDouble(input0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("User input must be a number.");
+                System.Threading.Thread.Sleep(3000);
+                Environment.Exit(-1);
+            }
+
+            // Get the number of checks written.
+            Console.WriteLine("How many checks were written? ");
+            string input1 = Console.ReadLine();
+            try
+            {
+                checks = Convert.ToInt32(input1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("User input must be a number.");
+                System.Threading.Thread.Sleep(3000);
+                Environment.Exit(-1);
+            }
+
+            // Create a BankCharges object.
+            BankCharges bc = new BankCharges(balance, checks);
+
+            // Display the fees.
+            Console.WriteLine("Bank fees: " + bc.GetFees().ToString("C"));
+            Console.ReadLine();
         }
     }
 }
